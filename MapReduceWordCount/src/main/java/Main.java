4243bc4d2491e7162a09ca9eaebc55bef5cf1881
sindhu-chirumamilla <sindhu.chirumamilla@gmail.com>
2021-10-28 21:20:52 -0500

    pushing mapreduce usecases

---
+++ MapReduceWordCount/src/main/java/Main.java
@@ -0,0 +1,58 @@
+import java.io.IOException;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+public class Main extends Configured implements Tool {
+    public static void main(String[] args) throws Exception{
+        int exitCode = ToolRunner.run(new Main(), args);
+        System.exit(exitCode);
+    }
+
+    public int run(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.printf("Usage: %s [generic options] <input> <output>\n",
+                    getClass().getSimpleName());
+            ToolRunner.printGenericCommandUsage(System.err);
+            return -1;
+        }
+
+        Job job = new org.apache.hadoop.mapreduce.Job();
+
+        job.setJarByClass(Main.class);
+        job.setJobName("WordCounter");
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+        job.setMapperClass(Map.class);
+        job.setReducerClass(Reduce.class);
+
+        //job.setNumReduceTasks(0);
+        /*
+        job.setNumReduceTasks(2);
+        job.setPartitionerClass(CustomPartitioner.class);
+        job.setCombinerClass(Reduce.class);
+        */
+
+
+
+
+        int returnValue = job.waitForCompletion(true) ? 0:1;
+        System.out.println("job.isSuccessful " + job.isSuccessful());
+        return returnValue;
+    }
+}
+
+
